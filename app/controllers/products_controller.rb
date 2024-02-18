6fb5af4d510c2364c617ea0edc5679b79bd26c5f
maeci <cimafrancamae@gmail.com>
2024-02-18 15:32:25 +0800

    Add style to index, new, and form html

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -12,7 +12,7 @@ class ProductsController < ApplicationController
     @product = Product.new(product_params)
 
     if @product.save
-      redirect_to @products_path, notice: 'Product was successfully created.'
+      redirect_to products_path, notice: 'Product was successfully created.'
     else
       render :new
     end
